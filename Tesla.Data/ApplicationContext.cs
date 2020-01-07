7d10c0b89a42c8269e059861f77de0764c02cc6f
jahangir mustafa <the.jahangir@gmail.com>
2020-01-07 11:00:41 +0500

    Initial committ

---
+++ Tesla.Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tesla.Data.Entities;
+
+namespace Tesla.Data
+{
+	public class ApplicationContext : DbContext
+	{
+		public ApplicationContext(DbContextOptions options)
+				: base(options)
+		{
+		}
+
+		public DbSet<Location> Locations { get; set; }
+		public DbSet<User> Users { get; set; }
+	}
+}
---
+++ Tesla/Migrations/20200106140902_AddLocationTab.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tesla.Migrations
+{
+    public partial class AddLocationTab : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Locations");
+        }
+    }
+}
